304dfc46db8c0a19753746e41294951beac5ce75
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-06-19 02:53:07 +0530

    .

--- new-tutorial/js/13.hoisting.js
+++ new-tutorial/js/13.hoisting.js
@@ -2,17 +2,17 @@
 'use strict'
 
 hello()
-function hello(){
-    console.log('This called hoisting')
+function hello() {
+  console.log('This called hoisting')
 }
 console.log() //can't access const vairiable before initilization
-const a = 'hey';
+const a = 'hey'
 console.log(a)
 
-let hey;
+let hey
 
-hey = 0 
+hey = 0
 console.log(hey)
 
-x = 3;
+x = 3
 console.log(x)
