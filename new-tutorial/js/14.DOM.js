79c18215a4d41653a060b2891318e022293a3e90
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-06-28 15:00:23 +0530

    style

--- new-tutorial/js/14.DOM.js
+++ new-tutorial/js/14.DOM.js
@@ -1,10 +1,19 @@
 // The Document Object Model is an Application programming inteface (API) for manipulating HTML and XML documents.b
-// let a = (document.getElementById('object').firstElementChild.style.color ='yellow')
-// // let b = document.getElementById('object')
-// console.log(a)
+let a = (document.getElementById('object').firstElementChild.style.color =
+  '#cbcc19')
+console.log(a)
 
-let className =  document.getElementsByClassName('txt')
-// console.log(className)
-for(let i=0;i<className.length;i++){
-    className[i].innerText = 'value'
+let className = document.querySelector('div').getElementsByTagName('input')
+// className = document.placeholder='h'
+console.log(className)
+for (let i = 0; i < className.length; i++) {
+  className[i].placeholder = 'a'
+}
+
+let className1 = document.querySelector('div').getElementsByTagName('input')
+for (const key in className1) {
+  className1[key].placeholder = 'Enter your information'
+  className1[key].style.borderRadius='90px'
+  className1[key].style.background='green'
+  className1[key].style.scale='0.9'
 }
