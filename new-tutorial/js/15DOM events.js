af37956b01057f9990e74b3f210e4bfa94199f6c
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-06 19:31:22 +0530

    Mouse events

--- new-tutorial/js/15DOM events.js
+++ new-tutorial/js/15DOM events.js
@@ -1,6 +1,8 @@
+let gId = document.getElementById('div')
 let creat = document.createElement('button')
+gId.append(creat)
 let txt = document.createTextNode('Click me ')
-creat.append(txt)
+let txtandele = creat.append(txt)
 // creat.setAttribute('onfocus', 'hello()')
 creat.setAttribute('ondblclick', 'hey()')
 creat.addEventListener('click', function () {
@@ -19,7 +21,6 @@ function hey() {
 }
 
 // removeEventListener is same work as work addEventListener
-
 window.addEventListener('DOMContentLoaded', function () {
   console.log('DOM tree created')
 })
@@ -27,5 +28,5 @@ window.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('load', function () {
   console.log('loaded')
 })
-let h = evnt.clientWidth
-console.log(h)
+
+// mouse Events
