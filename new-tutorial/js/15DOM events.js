f451eb513e47b2afe9481c26ab26b0562c92ab1f
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-06 14:33:49 +0530

    click and focus

--- new-tutorial/js/15DOM events.js
+++ new-tutorial/js/15DOM events.js
@@ -1,16 +1,20 @@
 let creat = document.createElement('button')
 let txt = document.createTextNode('Click me ')
 creat.append(txt)
-creat.setAttribute('onclick','hey()')
-creat.setAttribute('onfocus','hello()')
+// creat.setAttribute('onfocus', 'hello()')
+creat.setAttribute('ondblclick', 'hey()')
+creat.addEventListener('click', function() {
+  Addevnt.style.backgroundColor = 'transparent'
+})
+
+creat.addEventListener('mouseenter', function() {
+    Addevnt.style.backgroundColor = 'red'
+  })
 creat.setAttribute('class', 'btn cls')
 let evnt = document.body.appendChild(creat)
 console.log(evnt)
 let Addevnt = document.querySelector('.cls')
-function hey(){
-    // Addevnt.style.backgroundColor='yellow'
+function hey() {
+  Addevnt.style.backgroundColor = 'yellow'
 }
 
-function hello(){
-    Addevnt.style.backgroundColor='green'
-}
