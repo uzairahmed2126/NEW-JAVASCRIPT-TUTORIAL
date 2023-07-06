c73a86b403ebf705d3de6a246963f6ca7230eff6
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-06 16:11:12 +0530

    click and focus

--- new-tutorial/js/15DOM events.js
+++ new-tutorial/js/15DOM events.js
@@ -3,13 +3,13 @@ let txt = document.createTextNode('Click me ')
 creat.append(txt)
 // creat.setAttribute('onfocus', 'hello()')
 creat.setAttribute('ondblclick', 'hey()')
-creat.addEventListener('click', function() {
+creat.addEventListener('click', function () {
   Addevnt.style.backgroundColor = 'transparent'
 })
 
-creat.addEventListener('mouseenter', function() {
-    Addevnt.style.backgroundColor = 'red'
-  })
+creat.addEventListener('mouseenter', function () {
+  Addevnt.style.backgroundColor = 'green'
+})
 creat.setAttribute('class', 'btn cls')
 let evnt = document.body.appendChild(creat)
 console.log(evnt)
@@ -18,3 +18,12 @@ function hey() {
   Addevnt.style.backgroundColor = 'yellow'
 }
 
+// removeEventListener is same work as work addEventListener
+
+window.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM tree created')
+})
+
+window.addEventListener('load', function () {
+  console.log('loaded')
+})
