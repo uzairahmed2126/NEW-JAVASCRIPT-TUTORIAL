7eb818c761cc3f70d156a8e5cb8c1ba56d658076
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-10 16:22:47 +0530

    BOM

--- new-tutorial/js/16.BOM.js
+++ new-tutorial/js/16.BOM.js
@@ -3,7 +3,25 @@ console.log(window.outerWidth + ' outer width')
 console.log(window.innerHeight + ' inner height')
 console.log(window.innerWidth + ' inner width')
 
-let id = document.getElementById('btn');
-id.addEventListener('click' ,function(){
-    window.open('/new-tutorial/html/DOM events.html','DOM window',"height=500, width=500,font-family='Courier New', Courier, monospace")
+let id = document.getElementById('btn')
+id.addEventListener('click', function () {
+  window.open(
+    '/new-tutorial/html/DOM events.html',
+    'index.html',
+    "height=500, width=500,font-family='Courier New', Courier, monospace",
+  )
+})
+
+let id1 = document.getElementById('btn1')
+id1.addEventListener('click', function () {
+  window.open(
+    'https://www.google.com',
+    'newname',
+    "height=500, width=500,font-family='Courier New', Courier, monospace",
+  )
+})
+
+let id2 = document.getElementById('btn2')
+id2.addEventListener('click', function () {
+  window.close()
 })
