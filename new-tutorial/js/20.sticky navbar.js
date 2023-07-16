f7aa8adb0d1b9705c90e368b1e93948020b334ec
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-07-16 17:33:40 +0530

    Completed sticky NavBar

--- new-tutorial/js/20.sticky navbar.js
+++ new-tutorial/js/20.sticky navbar.js
@@ -1,5 +1,9 @@
 window.addEventListener('scroll',function(){
-    if(this.window.pageYOffset >= 240){
-        console.log('ok')
+    let navbar = this.document.getElementById('menu-bar')
+    if(window.pageYOffset >= 204){
+        navbar.classList.add('sticky')
+    }else{
+
+        navbar.classList.remove('sticky')
     }
 })
